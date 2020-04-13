53f485ed8f1c3f8a822faf524e255e4c37cf5d2b
programaker <programaker@gmail.com>
2020-04-13 19:21:10 +0200

    ZIO modules can be package objects, according to the How To doc

--- src/main/scala/spotification/spotify/authorization/package.scala
+++ src/main/scala/spotification/spotify/authorization/package.scala
@@ -3,22 +3,20 @@ package spotification.spotify
 import zio.{Has, IO, RIO, Task, ZIO}
 
 package object authorization {
-  type Authorization = Has[Authorization.Service]
+  type Authorization = Has[AuthorizationService]
 
-  object Authorization {
-    trait Service {
-      def authorize(req: AuthorizationRequest): IO[AuthorizationError, AuthorizationResponse]
-      def requestToken(req: TokenRequest): Task[TokenResponse]
-      def refreshToken(req: RefreshTokenRequest): Task[RefreshTokenResponse]
-    }
-
-    def authorize(req: AuthorizationRequest): ZIO[Authorization, AuthorizationError, AuthorizationResponse] =
-      ZIO.accessM(_.get.authorize(req))
-
-    def requestToken(req: TokenRequest): RIO[Authorization, TokenResponse] =
-      ZIO.accessM(_.get.requestToken(req))
-
-    def refreshToken(req: RefreshTokenRequest): RIO[Authorization, RefreshTokenResponse] =
-      ZIO.accessM(_.get.refreshToken(req))
+  trait AuthorizationService {
+    def authorize(req: AuthorizationRequest): IO[AuthorizationError, AuthorizationResponse]
+    def requestToken(req: TokenRequest): Task[TokenResponse]
+    def refreshToken(req: RefreshTokenRequest): Task[RefreshTokenResponse]
   }
+
+  def authorize(req: AuthorizationRequest): ZIO[Authorization, AuthorizationError, AuthorizationResponse] =
+    ZIO.accessM(_.get.authorize(req))
+
+  def requestToken(req: TokenRequest): RIO[Authorization, TokenResponse] =
+    ZIO.accessM(_.get.requestToken(req))
+
+  def refreshToken(req: RefreshTokenRequest): RIO[Authorization, RefreshTokenResponse] =
+    ZIO.accessM(_.get.refreshToken(req))
 }

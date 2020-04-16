ff21ee4016a411ff19fb4dd896437a9a8a8f4b74
programaker <programaker@gmail.com>
2020-04-16 14:27:13 +0200

    Imports

--- src/main/scala/spotification/spotify/authorization/client/package.scala
+++ src/main/scala/spotification/spotify/authorization/client/package.scala
@@ -4,12 +4,15 @@ import org.http4s.{AuthScheme, Credentials => H4sCredentials}
 import org.http4s.headers.{Authorization => H4sAuthorization}
 import eu.timepit.refined.auto._
 
+import H4sCredentials.Token
+import AuthScheme._
+
 package object client {
 
   def authorizationBasicHeader(credentials: Credentials): H4sAuthorization =
-    H4sAuthorization(H4sCredentials.Token(AuthScheme.Basic, base64Credentials(credentials)))
+    H4sAuthorization(Token(Basic, base64Credentials(credentials)))
 
   def authorizationBearerHeader(accessToken: AccessToken): H4sAuthorization =
-    H4sAuthorization(H4sCredentials.Token(AuthScheme.Bearer, accessToken.value))
+    H4sAuthorization(Token(Bearer, accessToken.value))
 
 }

f5e338d1b1d0009a18b8038d0ebf22a55ce11435
programaker <programaker@gmail.com>
2020-05-07 23:45:21 +0200

    Add the refresh-token in config (optionally, otherwise we couldn't run the app to get it in the first place)

--- src/main/scala/spotification/domain/config/SpotifyConfig.scala
+++ src/main/scala/spotification/domain/config/SpotifyConfig.scala
@@ -1,11 +1,12 @@
 package spotification.domain.config
 
-import spotification.domain.UriString
+import spotification.domain.{NonBlankString, UriString}
 import spotification.domain.spotify.authorization.{ClientId, ClientSecret, Scope}
 
 final case class SpotifyConfig(
   clientId: ClientId,
   clientSecret: ClientSecret,
   redirectUri: UriString,
-  scopes: Option[List[Scope]]
+  scopes: Option[List[Scope]],
+  refreshToken: Option[NonBlankString]
 )

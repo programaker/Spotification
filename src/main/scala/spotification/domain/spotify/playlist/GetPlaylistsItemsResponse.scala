90974c6a17989f9cc714b93eafd636d4d81f1e0c
programaker <programaker@gmail.com>
2020-05-16 14:36:27 +0200

    Trying a different namespacing

--- src/main/scala/spotification/domain/spotify/playlist/GetPlaylistsItemsResponse.scala
+++ src/main/scala/spotification/domain/spotify/playlist/GetPlaylistsItemsResponse.scala
@@ -1,6 +1,7 @@
 package spotification.domain.spotify.playlist
 
 import spotification.domain.spotify.album.{AlbumId, AlbumType}
+import spotification.domain.spotify.playlist.GetPlaylistsItemsResponse.Success.TrackResponse
 import spotification.domain.{PositiveInt, UriString}
 
 sealed abstract class GetPlaylistsItemsResponse extends Product with Serializable
@@ -10,12 +11,10 @@ object GetPlaylistsItemsResponse {
     total: PositiveInt,
     next: Option[UriString]
   ) extends GetPlaylistsItemsResponse
+  object Success {
+    final case class TrackResponse(album: AlbumResponse)
+    final case class AlbumResponse(id: AlbumId, album_type: AlbumType)
+  }
 
-  final case class Error(
-    status: Int,
-    message: String
-  ) extends GetPlaylistsItemsResponse
-
-  final case class TrackResponse(album: AlbumResponse)
-  final case class AlbumResponse(id: AlbumId, album_type: AlbumType)
+  final case class Error(status: Int, message: String) extends GetPlaylistsItemsResponse
 }

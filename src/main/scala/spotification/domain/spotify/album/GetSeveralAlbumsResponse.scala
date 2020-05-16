7c263c66d108ee2fd0c1d681ee9742e1e9325551
programaker <programaker@gmail.com>
2020-05-16 15:10:55 +0200

    Album types

---
+++ src/main/scala/spotification/domain/spotify/album/GetSeveralAlbumsResponse.scala
@@ -0,0 +1,16 @@
+package spotification.domain.spotify.album
+
+import spotification.domain.spotify.album.GetSeveralAlbumsResponse.Success.AlbumResponse
+import spotification.domain.spotify.track.TrackUri
+
+sealed abstract class GetSeveralAlbumsResponse extends Product with Serializable
+object GetSeveralAlbumsResponse {
+  final case class Success(albums: List[AlbumResponse]) extends GetSeveralAlbumsResponse
+  object Success {
+    final case class AlbumResponse(tracks: TracksResponse)
+    final case class TracksResponse(items: ItemResponse)
+    final case class ItemResponse(uri: TrackUri)
+  }
+
+  final case class Error(status: Int, message: String) extends GetSeveralAlbumsResponse
+}
--- src/main/scala/spotification/domain/spotify/album/package.scala
+++ src/main/scala/spotification/domain/spotify/album/package.scala
@@ -1,12 +1,12 @@
 package spotification.domain.spotify
 
 import eu.timepit.refined.api.Refined
-import eu.timepit.refined.boolean.Or
+import eu.timepit.refined.boolean.{And, Or}
 import eu.timepit.refined.generic.Equal
-import eu.timepit.refined.auto._
 import io.estatico.newtype.macros.newtype
 import spotification.domain.SpotifyId
 import cats.implicits._
+import eu.timepit.refined.collection.{MaxSize, MinSize}
 
 package object album {
   @newtype case class AlbumId(value: SpotifyId)
@@ -14,6 +14,11 @@ package object album {
   type AlbumTypeR = Equal["album"] Or Equal["single"] Or Equal["compilation"]
   type AlbumType = String Refined AlbumTypeR
   object AlbumType {
-    val isAlbum: AlbumType => Boolean = "album" === _
+    val isAlbum: AlbumType => Boolean = _.value === "album"
   }
+
+  // A maximum of 20 albums can be gotten at a time
+  // An IndexedSeq is being used due to efficient `length` operation (needed for the refinement)
+  type AlbumIdsToGetR = MinSize[1] And MaxSize[20]
+  type AlbumIdsToGet = IndexedSeq[AlbumId] Refined AlbumIdsToGetR
 }

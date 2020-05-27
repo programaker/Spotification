6938915a793806e7afeba4baffa020bf3bb6090e
Marcelo da Silva Gomes <marcelo.dasilvagomes@auto1.com>
2020-05-27 17:50:15 +0200

    Bringing http4s client back to H4sAuthorizationService

--- src/main/scala/spotification/infra/httpclient/H4sAuthorizationService.scala
+++ src/main/scala/spotification/infra/httpclient/H4sAuthorizationService.scala
@@ -3,10 +3,14 @@ package spotification.infra.httpclient
 import cats.implicits._
 import io.circe.generic.auto._
 import io.circe.jawn
+import org.http4s.Method._
+import org.http4s.{Uri, UrlForm}
 import spotification.domain.spotify.authorization._
 import zio.Task
+import zio.interop.catz._
 import eu.timepit.refined.auto._
 import HttpClient._
+import AuthorizationHttpClient._
 import spotification.domain.spotify.authorization.Authorization.base64Credentials
 import spotification.infra.httpclient.JHttpClient.jPost
 import spotification.infra.spotify.authorization.AuthorizationModule
@@ -20,7 +24,11 @@ import spotification.infra.spotify.authorization.AuthorizationModule
 import io.circe.refined._
 import spotification.infra.Json.Implicits._
 
-final class H4sAuthorizationService(apiTokenUri: ApiTokenUri) extends AuthorizationModule.Service {
+final class H4sAuthorizationService(apiTokenUri: ApiTokenUri, httpClient: H4sClient)
+    extends AuthorizationModule.Service {
+
+  import H4sClientDsl._
+
   override def requestToken(req: AccessTokenRequest): Task[AccessTokenResponse] = {
     val params: ParamMap = Map(
       "grant_type"   -> req.grant_type,
@@ -33,12 +41,14 @@ final class H4sAuthorizationService(apiTokenUri: ApiTokenUri) extends Authorizat
       "Content-Type"  -> "application/x-www-form-urlencoded; charset=UTF-8"
     )
 
+    // I hope this is the only request that will need to use
+    // Java as a secret weapon, due to the redirect_uri
     jPost(apiTokenUri.show, makeQueryString(params), headers)
       .map(jawn.decode[AccessTokenResponse])
       .flatMap(Task.fromEither(_))
   }
 
-  override def refreshToken(req: RefreshTokenRequest): Task[RefreshTokenResponse] = {
+  /*override def refreshToken(req: RefreshTokenRequest): Task[RefreshTokenResponse] = {
     val params: ParamMap = Map(
       "grant_type"    -> req.grant_type,
       "refresh_token" -> req.refresh_token.show
@@ -50,6 +60,23 @@ final class H4sAuthorizationService(apiTokenUri: ApiTokenUri) extends Authorizat
     )
 
     jPost(apiTokenUri.show, makeQueryString(params), headers)
+      .map(jawn.decode[RefreshTokenResponse])
+      .flatMap(Task.fromEither(_))
+  }*/
+  override def refreshToken(req: RefreshTokenRequest): Task[RefreshTokenResponse] = {
+    val urlForm = UrlForm(
+      "grant_type"    -> req.grant_type,
+      "refresh_token" -> req.refresh_token.show
+    )
+
+    val post = POST(
+      urlForm,
+      Uri.unsafeFromString(apiTokenUri.show),
+      authorizationBasicHeader(req.client_id, req.client_secret)
+    )
+
+    httpClient
+      .expect[String](post)
       .map(jawn.decode[RefreshTokenResponse])
       .flatMap(Task.fromEither(_))
   }
--- src/main/scala/spotification/infra/spotify/authorization/package.scala
+++ src/main/scala/spotification/infra/spotify/authorization/package.scala
@@ -3,7 +3,7 @@ package spotification.infra.spotify
 import spotification.domain.config.AuthorizationConfig
 import spotification.domain.spotify.authorization._
 import spotification.infra.config.AuthorizationConfigModule
-import spotification.infra.httpclient.{H4sAuthorizationService, HttpClientModule}
+import spotification.infra.httpclient.{H4sAuthorizationService, H4sClient, HttpClientModule}
 import zio._
 
 package object authorization {
@@ -15,9 +15,16 @@ package object authorization {
     def refreshToken(req: RefreshTokenRequest): RIO[AuthorizationModule, RefreshTokenResponse] =
       ZIO.accessM(_.get.refreshToken(req))
 
-    val layer: TaskLayer[AuthorizationModule] = {
+    /*val layer: TaskLayer[AuthorizationModule] = {
       val l1 = ZLayer.fromService[AuthorizationConfig, AuthorizationModule.Service] { config =>
         new H4sAuthorizationService(config.apiTokenUri)
+      }
+
+      (AuthorizationConfigModule.layer ++ HttpClientModule.layer) >>> l1
+    }*/
+    val layer: TaskLayer[AuthorizationModule] = {
+      val l1 = ZLayer.fromServices[AuthorizationConfig, H4sClient, AuthorizationModule.Service] {
+        (config, httpClient) => new H4sAuthorizationService(config.apiTokenUri, httpClient)
       }
 
       (AuthorizationConfigModule.layer ++ HttpClientModule.layer) >>> l1

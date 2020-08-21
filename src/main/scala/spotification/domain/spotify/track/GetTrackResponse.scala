faecb1a612fc6d76195cd8031d34e465021f8907
Marcelo da Silva Gomes <marcelo.dasilvagomes@auto1.com>
2020-08-21 17:34:14 +0200

    Add `external_urls` field to GetTrackResponse

--- src/main/scala/spotification/domain/spotify/track/GetTrackResponse.scala
+++ src/main/scala/spotification/domain/spotify/track/GetTrackResponse.scala
@@ -1,11 +1,13 @@
 package spotification.domain.spotify.track
 
-import spotification.domain.NonBlankString
+import spotification.domain.{NonBlankString, UriString}
 
 final case class GetTrackResponse(
   artists: List[GetTrackResponse.ArtistResponse],
+  external_urls: GetTrackResponse.ExternalUrls,
   name: NonBlankString
 )
 object GetTrackResponse {
   final case class ArtistResponse(name: NonBlankString)
+  final case class ExternalUrls(spotify: UriString)
 }

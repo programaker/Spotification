b4f9ce1fe9980bdc6c130d7d64068d2ecb55149a
programaker <programaker@gmail.com>
2020-12-31 00:30:57 +0100

    Add decoders

--- src/main/scala/spotification/album/json/implicits.scala
+++ src/main/scala/spotification/album/json/implicits.scala
@@ -6,8 +6,10 @@ import io.circe.refined.refinedDecoder
 import io.estatico.newtype.ops.toCoercibleIdOps
 import spotification.album.{AlbumId, GetAlbumSampleTrackResponse}
 import spotification.common.SpotifyId
+import spotification.track.json.implicits.TrackIdDecoder
 
 object implicits {
   implicit val AlbumIdDecoder: Decoder[AlbumId] = implicitly[Decoder[SpotifyId]].map(_.coerce[AlbumId])
+  implicit val GetAlbumSampleTrackDecoder: Decoder[GetAlbumSampleTrackResponse.Track] = deriveDecoder
   implicit val GetAlbumSampleTrackResponseDecoder: Decoder[GetAlbumSampleTrackResponse] = deriveDecoder
 }
--- src/main/scala/spotification/track/json/implicits.scala
+++ src/main/scala/spotification/track/json/implicits.scala
@@ -3,9 +3,12 @@ package spotification.track.json
 import io.circe.Decoder
 import io.circe.generic.semiauto.deriveDecoder
 import io.circe.refined.refinedDecoder
-import spotification.track.GetTrackResponse
+import io.estatico.newtype.ops.toCoercibleIdOps
+import spotification.common.SpotifyId
+import spotification.track.{GetTrackResponse, TrackId}
 
 object implicits {
+  implicit val TrackIdDecoder: Decoder[TrackId] = implicitly[Decoder[SpotifyId]].map(_.coerce[TrackId])
   implicit val GetTrackResponseArtistDecoder: Decoder[GetTrackResponse.Artist] = deriveDecoder
   implicit val GetTrackResponseExternalUrlsDecoder: Decoder[GetTrackResponse.ExternalUrls] = deriveDecoder
   implicit val GetTrackResponseDecoder: Decoder[GetTrackResponse] = deriveDecoder

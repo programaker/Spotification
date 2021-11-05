2bafac7198f49ec5ec4450888eb47296cc187d11
Marcelo Gomes <marcelo.gomes@commercetools.com>
2021-11-05 19:13:29 +0100

    Fix album httpclient

--- src/main/scala/spotification/album/httpclient/httpclient.scala
+++ src/main/scala/spotification/album/httpclient/httpclient.scala
@@ -3,15 +3,15 @@ package spotification.album
 import cats.syntax.show._
 import eu.timepit.refined.cats._
 import org.http4s.Method.GET
-import org.http4s.Uri
+import org.http4s.{EntityDecoder, Uri}
 import spotification.album.json.implicits.GetAlbumSampleTrackResponseDecoder
 import spotification.album.service.{GetAlbumSampleTrackService, GetAlbumSampleTrackServiceR}
 import spotification.authorization.httpclient.authorizationBearerHeader
 import spotification.common.httpclient.{H4sClient, HttpClientR, doRequest, uriStringToUri}
-import spotification.common.json.implicits.ErrorResponseDecoder
+import spotification.common.json.implicits.{ErrorResponseDecoder, entityDecoderF}
 import spotification.config.AlbumConfig
 import spotification.config.service.AlbumConfigR
-import zio.interop.catz.monadErrorInstance
+import zio.interop.catz.concurrentInstance
 import zio.{Task, URLayer, ZLayer}
 
 package object httpclient {
@@ -19,6 +19,8 @@ package object httpclient {
 
   val GetAlbumSampleTrackServiceLayer: URLayer[AlbumConfigR with HttpClientR, GetAlbumSampleTrackServiceR] =
     ZLayer.fromServices[AlbumConfig, H4sClient, GetAlbumSampleTrackService] { (config, http) => req =>
+      implicit val ed: EntityDecoder[Task, String] = entityDecoderF
+
       val h4sUri = albumsTracksUri(config.albumApiUri, req.albumId).flatMap(uriStringToUri).map {
         _.withQueryParam("limit", req.limit.show)
           .withQueryParam("offset", req.offset.show)

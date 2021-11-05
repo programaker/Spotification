8438d9ed9ef2a1d3fcd1e5bccf2b41676668a0ed
Marcelo Gomes <marcelo.gomes@commercetools.com>
2021-11-05 19:05:47 +0100

    Fix entityDecoderF

--- src/main/scala/spotification/common/json/implicits.scala
+++ src/main/scala/spotification/common/json/implicits.scala
@@ -1,15 +1,15 @@
 package spotification.common.json
 
-import cats.effect.Sync
-import io.circe.{Decoder, Encoder}
+import cats.effect.Concurrent
 import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
+import io.circe.{Decoder, Encoder}
 import org.http4s.circe.{jsonEncoderOf, jsonOf}
 import org.http4s.{EntityDecoder, EntityEncoder}
 import spotification.common.{ErrorResponse, GenericResponse}
 
 object implicits {
   implicit def entityEncoderF[F[_], A: Encoder]: EntityEncoder[F, A] = jsonEncoderOf
-  implicit def entityDecoderF[F[_]: Sync, A: Decoder]: EntityDecoder[F, A] = jsonOf
+  implicit def entityDecoderF[F[_]: Concurrent, A: Decoder]: EntityDecoder[F, A] = jsonOf
 
   implicit val ErrorResponseDecoder: Decoder[ErrorResponse] = deriveDecoder
   implicit val GenericResponseSuccessEncoder: Encoder[GenericResponse.Success] = deriveEncoder
